acce97d45b74b36aa8351d9ae3607634c95ce47a
Carlos <carlosdiaaz28@gmail.com>
2026-08-27 14:01:58 -0600

    Agrega programa a control de acceso

---
+++ acceso.py
@@ -0,0 +1,12 @@
+
+nombre = input("cual es su nombre")
+edad = int (input("ingrese su edad"))
+if edad <0 or edad >120:
+    print ("Edad invalida por favor introduzca edad valida")    
+elif edad <=17:
+ print("Acceso denegado")
+else:
+    print("Acceso permitido")
+
+
+

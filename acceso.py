dea655d31c47948473d0c8f6251e2b64a0f31ae8
Carlos <carlosdiaaz28@gmail.com>
2026-08-31 13:40:53 -0600

    Mejora validacion de nombre y edad

--- acceso.py
+++ acceso.py
@@ -1,12 +1,28 @@
+while True:
+    nombre = input("Cual es su nombre: ")
+    if nombre.isdigit() or nombre == "":
 
-nombre = input("cual es su nombre")
-edad = int (input("ingrese su edad"))
+     print("debe ingresar su nombre")
+    else:
+        break
+while True:
+    try:
+        edad = int(input("Ingrese su edad: "))
+        break
+    except ValueError:
+    
+     print("Valor incorrecto. Ingrese su edad sin letras ni decimales. Ejemplo: 18")     
+
 if edad <0 or edad >120:
-    print ("Edad invalida por favor introduzca edad valida")    
+     print ("Edad invalida por favor introduzca edad valida")     
 elif edad <=17:
- print("Acceso denegado")
+     print("Acceso denegado")
 else:
     print("Acceso permitido")
+    
+
+   
+
 
 
 
